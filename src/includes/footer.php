a8c3f8bfaafc38a7fe793f15ef5094ca7bf2767a
Hector3144 <+>
2024-06-01 13:18:59 -0500

    ajustes

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -5,11 +5,8 @@
 </div>
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2023 <a href="#">Sistemas Free</a>.</strong>
+    <strong>Copyright &copy; 2024 <a href="#">Maycrosoft</a>.</strong>
     All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
-    </div>
 </footer>
 </div>
 <!-- ./wrapper -->
